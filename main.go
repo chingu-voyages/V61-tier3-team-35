b207d022f642960ee4f85b360ae9bfc241eb32d0
Camille Onoda <info@camilleonoda.com>
2026-06-24 11:07:33 +0900

    fix: remove commented-out code blocks

--- main.go
+++ main.go
@@ -8,20 +8,12 @@ import (
 	"github.com/joho/godotenv"
 )
 
-/*type apiConfig struct {
-	baseURL string
-}*/
-
 func main() {
 	godotenv.Load()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
-
-	/*	_ = &apiConfig{
-		baseURL: os.Getenv("BASE_URL"),
-	}*/
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("GET /admin/health", handlerReadiness)
