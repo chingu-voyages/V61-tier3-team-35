95212531c3203c064a2111675ff817501638250c
Camille Onoda <info@camilleonoda.com>
2026-07-03 11:14:41 +0900

    fix(cors): remove a trailing slash in allowedOrigins

--- internal/api/cors_middleware.go
+++ internal/api/cors_middleware.go
@@ -4,8 +4,8 @@ import "net/http"
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	allowedOrigins := map[string]bool{
-		"http://localhost:5173":                  true,
-		"https://v61-tier3-team-35.netlify.app/": true,
+		"http://localhost:5173":                 true,
+		"https://v61-tier3-team-35.netlify.app": true,
 	}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -17,6 +17,7 @@ func CorsMiddleware(next http.Handler) http.Handler {
 
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
