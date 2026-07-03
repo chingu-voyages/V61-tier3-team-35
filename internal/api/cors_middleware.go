afa9de7ee350c2d465d4d2f09ccc219965869d91
Camille Onoda <info@camilleonoda.com>
2026-07-03 10:50:32 +0900

    feat(frontend): added the frontend deployment link to the list of allowed origins

--- internal/api/cors_middleware.go
+++ internal/api/cors_middleware.go
@@ -4,7 +4,8 @@ import "net/http"
 
 func CorsMiddleware(next http.Handler) http.Handler {
 	allowedOrigins := map[string]bool{
-		"http://localhost:5173": true,
+		"http://localhost:5173":                  true,
+		"https://v61-tier3-team-35.netlify.app/": true,
 	}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
